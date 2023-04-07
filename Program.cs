1cc733179fcfb2e5d4ff36e4224cd9d1b7919a1c
Davit <dav0709@icloud.com>
2023-04-07 19:11:23 +0400

    uxagrum

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.Write("Введите число M: ");
+    int M = int.Parse(Console.ReadLine());
+    Console.Write("Введите число N: ");
+    int N = int.Parse(Console.ReadLine());
+    int sum = 0;
+
+    for (int i = M; i <= N; i++) {
+      sum += i;
+    }
+
+    Console.WriteLine("Сумма натуральных чисел от {0} до {1} равна {2}.", M, N, sum);
+  }
+}
+
